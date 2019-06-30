4d44bdb1b21568184ef6a10e413695988da85a72
Falafel72 <kylehu2000@gmail.com>
2019-07-01 03:29:06 +0800

    refactor: memes are now loaded in App component so they are only loaded once

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,49 @@
 import React from 'react';
+import axios from 'axios';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import './App.css';
 import MemeGenerator from './pages/MemeGenerator';
 
 /** Main app controller */
-function App() {
-  return (
-    <Router>
-      <div className="App">
-      <NavBar />
-      <Route exact={true} path='/' component={MemeGenerator} />
-      <Route path="/gallery" />
-      </div>
-    </Router>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    /* 
+      TODO: 
+        - shrink the images down by around half
+        - add search functionality (search by meme name)
+        - add gallery for meme templates
+    */ 
+    /* get meme data via get request */
+    axios.get('https://api.imgflip.com/get_memes')
+      .then(response => { 
+        if (response.data.success) {
+          let memes = response.data.data.memes; 
+          this.setState({
+            memeArray: memes
+          });
+        }
+      });
+    this.state = {
+      memeArray: null
+    }
+  } 
+  render() {
+    return (
+      <Router>
+        <div className="App">
+        <NavBar />
+        <Route 
+          exact={true}
+          path='/' 
+          render = {(routeProps) => 
+            <MemeGenerator {...routeProps} memeArray={this.state.memeArray} />
+          } />
+        <Route path="/gallery" />
+        </div>
+      </Router>
+    );
+  }
 }
 
 /** Component for navigational buttons */
--- src/App/pages/MemeGenerator.js
+++ src/App/pages/MemeGenerator.js
@@ -1,43 +1,35 @@
 import React from 'react';
-import axios from 'axios';
 
 /** Component that handles the meme generator */
 class MemeGenerator extends React.Component {
   constructor() {
     super();
-    /* get meme data via get request */
-    /* 
-      TODO: perhaps load the api in some superclass so we
-      only load all the memes once and not every time the page is reloaded
-    */
-    axios.get('https://api.imgflip.com/get_memes')
-      .then(response => { 
-        if (response.data.success) {
-          console.log(response.data.data.memes);
-          let memes = response.data.data.memes; 
-          this.setState({
-            memeArray: memes
-          });
-        }
-      });
       
     this.state = {
-      currentMemeIdx: 0,
+      currentMemeIdx: 1,
       memeArray: null
     }
   }
 
   render() {
     return( 
-      <div className="meme-gen">
-        <img src={
-          this.state.memeArray ? 
-          this.state.memeArray[this.state.currentMemeIdx].url
-          : ""
-        } alt="Loading..."></img>
-        <form className="search" >
-          <input type="text"></input>
-        </form>
+      // align left 
+      <div className='meme-gen'>
+        <div className="img-preview">
+          <img src={
+            this.props.memeArray ? 
+            this.props.memeArray[this.state.currentMemeIdx].url
+            : ''
+          } alt='Loading...'></img>
+        </div>
+        {/* align right */}
+        <div className='template-search' >
+          <input id='search' type='text'></input>
+          <div id='catalogue'>
+            <h2>MEME TITLE</h2>
+            {/* list of images go here */}
+          </div>
+        </div>
       </div>
     );
   }
