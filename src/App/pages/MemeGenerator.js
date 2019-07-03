b308c62a650d6816b7b86e8621f9ddb0f5e73c24
Falafel72 <kylehu2000@gmail.com>
2019-07-04 03:06:37 +0800

    image is now rendered in canvas, meme name is also displayed

--- src/App/pages/MemeGenerator.js
+++ src/App/pages/MemeGenerator.js
@@ -6,30 +6,94 @@ class MemeGenerator extends React.Component {
     super();
       
     this.state = {
-      currentMemeIdx: 1,
-      memeArray: null
+      currentMemeIdx: 0,
+      imgWidth: 0, 
+      imgHeight: 0,
+      memeName: "test"
     }
+
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(event) {
+    this.setState({memeName: event.target.value})
   }
 
   render() {
+    let imgObj = this.props.memeArray ? this.props.memeArray[this.state.currentMemeIdx] : null;
     return( 
-      // align left 
       <div className='meme-gen'>
+        {/* align left  */}
         <div className="img-preview">
-          <img src={
+          {/* <img src={
             this.props.memeArray ? 
             this.props.memeArray[this.state.currentMemeIdx].url
             : ''
-          } alt='Loading...'></img>
-        </div>
+          } alt='Loading...'></img> */}
+          <Canvas imgObj={imgObj} />
         {/* align right */}
+        </div>
         <div className='template-search' >
-          <input id='search' type='text'></input>
+          <input id='search' type='text' onChange={this.handleInput}></input>
           <div id='catalogue'>
-            <h2>MEME TITLE</h2>
-            {/* list of images go here */}
+            <h2>{imgObj ? imgObj.name : "Loading..."}</h2>
+            {/* <h2>{this.state.memeName}</h2> */}
+            <div id='meme-templates'>
+              {/* {this.props.memeArray.map()} */}
+            </div>
           </div>
         </div>
+      </div>
+    );
+  }
+}
+
+/** 
+ * Component for rendering all canvas elements
+ * May be used to also draw text
+ */
+class Canvas extends React.Component {
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef();
+  }
+
+  drawImage() {
+    const canvas = this.canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    if (this.props.imgObj) {
+      let img = new Image();
+      img.onload = () => {ctx.drawImage(img,0,0,img.width,img.height,
+                                            0,0,canvas.width,canvas.height)};
+      img.src = this.props.imgObj.url;
+    }
+  }
+
+  componentDidMount() {
+    this.drawImage();
+  }
+
+  componentDidUpdate() {
+    this.drawImage();
+  }
+
+  render() {
+    let width;
+    let height;
+    if (this.props.imgObj) {
+      width = this.props.imgObj.width/2;
+      height = this.props.imgObj.height/2;
+    } else {
+      width = 0;
+      height = 0;
+    }
+    return (
+      <div>
+        <canvas 
+          width={width} 
+          height={height}
+          ref={this.canvasRef}
+        />
       </div>
     );
   }
