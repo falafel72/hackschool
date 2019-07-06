a52f6d052cce389dd4815f8f791a72fa7068e6ff
Daniel Truong <ddtruong06@gmail.com>
2019-07-06 01:24:22 -0700

    Added simple MongoDB database to the server

--- server/app.js
+++ server/app.js
@@ -1,13 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// url routes 
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+// MongoDB database
+const mongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/memedb";
+
+const app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -37,5 +43,16 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+/** Initially creates the MongoDB database **/
+mongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log("Database has been created!");
+
+    // TODO(daniel): Create the collections and fields 
+
+
+    db.close();
+})
 
 module.exports = app;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var num = 0;
+
 /* GET users listing. */
 router.get('/', function(req, res) {
   res.send(JSON.stringify({memeArray: null, number: num}))
